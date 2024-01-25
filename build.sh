fd88bda99ee3bc8b5e1609dc581748052c96a8bc
Giedrius <giedrius@blokas.io>
2024-01-25 08:27:30 +0000

    pd-blokas 0.1.1-4, restructured build scripts for aarch64 compatibility.

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-mkdir -p build
-cd build
-
-cmake ../MEC -DDISABLE_LIBUSB=1 -DDISABLE_SOUNDPLANELITE=1 -DDISABLE_PUSH2=1 -DDISABLE_EIGENHARP=1 -DCPU_OPTIMIZATION_FLAGS=""
-
-make KontrolRack KontrolModule -j4
